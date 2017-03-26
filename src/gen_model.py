dcde86fbf922051339c6ffbe9999b0bd19b37694
tdeason416 <tdeason416@gmail.com>
2017-03-25 22:14:54 -0700

    evaluated model

--- src/gen_model.py
+++ src/gen_model.py
@@ -1,12 +1,14 @@
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
-import itertools
-import os
 plt.style.use('ggplot')
+import itertools
+import datetime
 import sklearn.model_selection as ms
 import sklearn.linear_model as lm
 import sklearn.ensemble as ens
+import os
+
 
 def _make_dummy(df, cols):
     df_ = df.copy()
@@ -53,42 +55,9 @@ def load_and_clean(csv_name):
                     'out_prncp_inv', 'revol_bal', 'pymnt_plan', 'issue_d', 'loan_status'], axis=1, inplace=True)
     rows_need_dummies = ['emp_title', 'home_ownership', 'emp_length', 'term', 'zip_code']
     loans_model = _make_dummy(loans_clean_, rows_need_dummies)
-    return loans_clean, loans_y
+    return loans_model, loans_y
 
-def generate_model(SklearnClass, dfx, dfy, test_holdout=False, **kwargs):
-    '''
-    generates model with train test split if needed
-    --------
-    Parameters:
-    SKlearn classifer - sklearn class
-    dfx: pd.dataframe containing labeled data
-    dfy: pd.dataframe containing data labels
-    --------
-    Returns:
-    model: fitted model
-    mdata: dictionary of labeled data
-    '''
-    mdata = {}
-    if test_holdout:
-        Xtr, Xte, ytr, yte = ms.train_test_split(dfx, dfy, test_size=test_holdout)
-        mdata['Xtr'] = Xtr
-        mdata['Xte'] = Xte
-        mdata['ytr'] = ytr
-        mdata['yte'] = yte
-    else:
-        mdata['Xtr'] = dfx
-        mdata['ytr'] = ytr
-    model = SklearnClass(kwargs)
-    model.fit(mdata['Xtr'], mdata['ytr'])
-    return probs, mdata
-    
-def generate_probs(model, mdata):
-    '''
-    generates predicted probabilities
-    '''
-    return model.predict_proba(mdata['Xtr'])
-
-def generate_roc_curve(mdata, probs, n_iter= 500, curve_name=False):
+def generate_roc_curve(labels, probs, n_iter=500, curve_name=False):
     '''
     mdata must contain matched yte values
     --------
@@ -103,7 +72,7 @@ def generate_roc_curve(mdata, probs, n_iter= 500, curve_name=False):
     thres_list = np.linspace(0,1,n_iter)
     thres_list = (thres_list * thres_list)
     rocs = []
-    test = yte.reshape(-1,1)
+    test = labels.reshape(-1,1)
     proba = probs.reshape(-1,1)
     pframe = pd.DataFrame(np.append(proba, test, axis=1), columns= ['predict', 'label'])
     c_true = pframe[pframe['label'] == 1]
@@ -118,11 +87,79 @@ def generate_roc_curve(mdata, probs, n_iter= 500, curve_name=False):
     for row in rocs:
         fpr.append(row['fpr'])
         tpr.append(row['tpr'])
-    print len(fpr)
-    ax.scatter(fpr, tpr, color=colors.next())
-    ax.plot(np.linspace(0,1,10), np.linspace(0,1,10))
+    ax.scatter(fpr, tpr, color=colors.next());
+    ax.plot(np.linspace(0,1,10), np.linspace(0,1,10));
+    ax.set_xlim(0,1);
+    ax.set_ylim(0,1);
+    ax.legend
+    fig.savefig('images/ROC-{}.png'.format(curve_name))
+    return rocs
+
+colors = itertools.cycle(['green', 'red', 'cyan', 'orange', 'violet',
+                          'yellow', 'blue', 'pink'])
+
+
+def roc_curve_many(json_folder, save_file=True):
+    '''
+    does the same as roc curve but for many items
+    '''
+    if json_folder[-1] != '/':
+        json_folder += '/'
+    colors  = itertools.cycle(['red','cyan', 'green', 'orange', 'violet', 'blue', 'pink'])
+    if save_file:
+        save_file = '../images/roc_curve-{}'.format(datetime.datetime.now().toordinal())
+    list_of_pd_frames = os.listdir(json_folder)
+    fig, ax = plt.subplots(1,1,figsize=(13,13))
+    for frame in list_of_pd_frames:
+        model = pd.read_json(json_folder+frame)
+        model['delinq_2yrs'] = model['delinq_2yrs'].fillna(0)
+        model['delinq_2yrs'] = model['delinq_2yrs'].apply(lambda x: 1 if x > 0 else 0)
+        model.columns = ['probability', 'label']
+        roc_values = []
+        for thres in np.linspace(.01, .99, 500):
+            model['pred'] = model['probability'].apply(lambda x: 1 if x > thres else 0)
+            rowdict = {}
+            is_pos = model[model['label'] == 1]
+            is_neg = model[model['label'] == 0]
+            rowdict['tpr'] = float(is_pos[is_pos['pred'] == 1].shape[0])/is_pos.shape[0]
+            rowdict['fpr'] = float(is_neg[is_neg['pred'] == 1].shape[0])/is_neg.shape[0]
+            roc_values.append(rowdict)
+        roc_values = pd.DataFrame(roc_values)
+        ax.scatter(roc_values['fpr'], roc_values['tpr'], color =colors.next(),label=frame)
+    ax.plot(np.linspace(0, 1, 20), np.linspace(0, 1, 20), color='black')
+    ax.legend()
     ax.set_xlim(0,1)
     ax.set_ylim(0,1)
+    fig.savefig(save_file)
 
-    fig.savefig('../images/ROC-{}.png')
-    return rocs
+# loans, y = load_and_clean('loan.csv')
+
+# model = ens.GradientBoostingClassifier(learning_rate=.1,n_estimators=50, max_depth=2)
+# Xtr,Xte, ytr, yte = ms.train_test_split(loans, y)
+# model.fit(Xtr, ytr)
+
+# probs = model.predict_proba(Xte)
+# probs = probs[:,1]
+
+# generate_roc_curve(yte, probs, curve_name='GBR500')
+
+# for num in np.arange(1,205,40):
+#     model = ens.RandomForestClassifier(n_estimators=num, max_depth=20,
+#                                                   n_jobs=-1)
+#     Xtr,Xte, ytr, yte = ms.train_test_split(loans, y)
+#     model.fit(Xtr, ytr)
+#     probs = model.predict_proba(Xte)
+#     probs = probs[:,1]
+#     probs = pd.Series(probs)
+#     labels= pd.Series(yte)
+#     pd.concat([probs,labels], axis=1).to_json('../data/{}_tree_rf'.format(num))
+
+# for num in np.arange(1,205,40):
+#     model = ens.GradientBoostingClassifier(learning_rate=.1, n_estimators=num, max_depth=2)
+#     Xtr,Xte, ytr, yte = ms.train_test_split(loans, y)
+#     model.fit(Xtr, ytr)
+#     probs = model.predict_proba(Xte)
+#     probs = probs[:,1]
+#     probs = pd.Series(probs)
+#     labels= pd.Series(yte)
+#     pd.concat([probs,labels], axis=1).to_json('../data/{}_tree_GBT'.format(num))
